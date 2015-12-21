afed08192c77400c370eb98889cdaccd916f2025
huy-tran <huy.tran1189@gmail.com>
2015-12-21 16:12:29 +1000

    Switch to ES6 syntax, Add Test Alarm button

--- App.js
+++ App.js
@@ -2,72 +2,92 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './Button';
 
-var App = React.createClass({
-    getInitialState: function(){
-        return {
+const POMODORO_TIMESET = 1500;
+const LONG_BREAK = 600
+const SHORT_BREAK = 300;
+const TIMER_INTEVAL = 1000;
+const TEST_ALARM = 3;
+
+class App extends React.Component{
+    constructor(){
+        super();
+        // Initial states
+        this.state = {
             running: false,
-            secondsElapsed: 1500,
-            choosenTime: 1500
-        };
-    },
-    formattedTime: function(sec){
-        var seconds = ('0' + sec % 60).slice(-2);
-        var minutes = Math.floor(sec / 60);
-        return (minutes + ":" + seconds);
-    },
-    clickStart: function(){
-        this.setState({ running: true });
-        this.interval = setInterval(function(){
-            this.setState({ secondsElapsed: this.state.secondsElapsed - 1 });
-            document.title = "Pomodoro Timer (" +  this.formattedTime(this.state.secondsElapsed) + ")";
-            if (this.state.secondsElapsed == 0) {
-                document.getElementById('bngAlarm').play();
-                clearInterval(this.interval);
-                this.setState({ 
-                    running: false,
-                    secondsElapsed: this.state.choosenTime 
-                });
-            }
-        }.bind(this), 1000);
-    },
-    clickStop: function(){
-        this.setState({ running: false });
-        clearInterval(this.interval);
-    },
-    clickReset: function(){
-        this.setState({ secondsElapsed: this.state.choosenTime });
-    },
-    setTimer: function(evt){
-        var type = evt.target.getAttribute("name");
-        switch(type){
-            case "Pomodoro": this.setState({ secondsElapsed: 1500, choosenTime: 1500 }); break;
-            case "Short Break": this.setState({ secondsElapsed: 300, choosenTime: 300 }); break;
-            case "Long Break": this.setState({ secondsElapsed: 600, choosenTime: 600 }); break;
+            secondsElapsed: POMODORO_TIMESET,
+            chosenTime: POMODORO_TIMESET
         }
-    },
-    render: function(){
+        this.setTimer = this.setTimer.bind(this);
+        this.countdown = this.countdown.bind(this);
+    }
+    // Time Format
+    formattedTime(sec){
+        let seconds = ('0' + sec % 60).slice(-2);
+        let minutes = Math.floor(sec / 60);
+        return `${minutes}:${seconds}`;
+    }
+    // Count down function
+    countdown(){
+        this.setState({ secondsElapsed: this.state.secondsElapsed - 1 });
+        document.title = `Pomodoro Timer (${this.formattedTime(this.state.secondsElapsed)})`; // Update document title based on elapsed second
+
+        if (this.state.secondsElapsed !== 0) {
+            this.timeout = setTimeout(this.countdown, 1000);
+        } else {
+            document.getElementById('bngAlarm').play(); // Trigger alarm sound when finish countdown
+            clearTimeout(this.timeout); 
+            this.setState({ 
+                running: false,
+                secondsElapsed: this.state.chosenTime // Reset time to chosen time
+            });
+        }
+    }
+    clickStart(){
+        this.setState({ running: true}); 
+        this.timeout = this.countdown();
+    }
+    clickStop(){
+        this.setState({ running: false });
+        clearTimeout(this.timeout);
+    }
+    clickReset(){
+        this.setState({ secondsElapsed: this.state.chosenTime });
+    }
+    setTimer(evt){
+        let type = evt.target.getAttribute("name");
+        switch(type){
+            case "Pomodoro": this.setState({ secondsElapsed: POMODORO_TIMESET, chosenTime: POMODORO_TIMESET }); break;
+            case "Short Break": this.setState({ secondsElapsed: SHORT_BREAK, chosenTime: SHORT_BREAK }); break;
+            case "Long Break": this.setState({ secondsElapsed: LONG_BREAK, chosenTime: LONG_BREAK }); break;
+            case "Test Alarm": this.setState({ secondsElapsed: TEST_ALARM, chosenTime: TEST_ALARM }); break;
+        }
+    }
+    render(){
+        let controlButtons = ['Pomodoro', 'Short Break', 'Long Break', 'Test Alarm'].map( (label, index) => {
+            return <Button key={index} name={label} handleClick={this.setTimer} className="me-btn-alt"/>
+        })
+
         return (
             <div>
                 <div className="timer-mode">
-                    <Button name="Pomodoro" handleClick={this.setTimer} className="me-btn-alt"/>
-                    <Button name="Short Break" handleClick={this.setTimer} className="me-btn-alt"/>
-                    <Button name="Long Break" handleClick={this.setTimer} className="me-btn-alt"/>
+                    {controlButtons}
                 </div>
                 <h2>{this.formattedTime(this.state.secondsElapsed)}</h2>
                 <div className="timer-controller">
                     {!this.state.running
-                        ? <Button name="Start" handleClick={this.clickStart} className="me-btn btn-start"/>
-                        : <Button name="Stop" handleClick={this.clickStop} className="me-btn btn-stop"/>
+                        ? <Button name="Start" handleClick={this.clickStart.bind(this)} className="me-btn btn-start"/>
+                        : <Button name="Stop" handleClick={this.clickStop.bind(this)} className="me-btn btn-stop"/>
                     }
-                    {!this.state.running && this.state.secondsElapsed !== this.state.choosenTime
-                        ? <Button name="Reset" handleClick={this.clickReset} className="me-btn btn-reset"/>
+                    {!this.state.running && this.state.secondsElapsed !== this.state.chosenTime
+                        ? <Button name="Reset" handleClick={this.clickReset.bind(this)} className="me-btn btn-reset"/>
                         : null
                     }
                 </div>
             </div>
         );
     }
-});
+    
+}
 
 ReactDOM.render(<App />, document.getElementById('bngTimer'));
 export default App;
--- Button.js
+++ Button.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-var Button = React.createClass({
-    render: function(){
-        return (
-            <button {...this.props} onClick={this.props.handleClick} className={this.props.className} >{this.props.name}</button>
-        );
-    }
-});
+const Button = (props) => { return <button {...props} onClick={props.handleClick} className={props.className} >{props.name}</button> }
 
 export default Button;
