321355e6f62921bc925ab851162fcfe3cb399845
Huy Tran <huy-tran@users.noreply.github.com>
2016-01-09 23:18:25 +1000

    Fix missing curly brace

--- Button.js
+++ Button.js
@@ -2,5 +2,6 @@ import React from 'react';
 
 const Button = (props) => { 
   return <button {...props} onClick={props.handleClick} className={props.className} >{props.name}</button>
+}
 
 export default Button;
