8afe912bacc6ad5e11f3dacdd256e2ddb9733583
huy-tran <huy.tran1189@gmail.com>
2016-01-09 23:15:31 +1000

    Tidy up

--- Button.js
+++ Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
-const Button = (props) => { return <button {...props} onClick={props.handleClick} className={props.className} >{props.name}</button> }
+const Button = (props) => { 
+  return <button {...props} onClick={props.handleClick} className={props.className} >{props.name}</button>
 
 export default Button;
